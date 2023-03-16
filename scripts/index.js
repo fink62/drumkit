6c113cf3c88b3f19c46a0db8d9e0b48095069d6a
Jörg Finkeisen <joerg@finkeisen.net>
2023-03-16 20:57:49 +0100

    keystrokes

--- scripts/index.js
+++ scripts/index.js
@@ -2,8 +2,47 @@ for (let i = 0; i < document.querySelectorAll(".drum").length; i++){
     document.querySelectorAll(".drum")[i].addEventListener("click", clickHandler);
 }
 
+document.addEventListener("keydown", (event) => {
+    switch (event.key) {
+        case "w":
+            var crash = new Audio('sounds/crash.mp3');
+            crash.play();
+            break;
+        case "a":
+            var kick = new Audio('sounds/kick-bass.mp3');
+            kick.play();
+            break;
+        case "s":
+            var snare = new Audio('sounds/snare.mp3');
+            snare.play();
+            break;
+        case "d":
+            var tom1 = new Audio('sounds/tom-1.mp3');
+            tom1.play();
+            break;
+        case "j":
+            var tom2 = new Audio('sounds/tom-2.mp3');
+            tom2.play();
+            break;
+        case "k":
+            var tom3 = new Audio('sounds/tom-3.mp3');
+            tom3.play();
+            break;
+        case "l":
+            var tom4 = new Audio('sounds/tom-4.mp3');
+            tom4.play();
+            break;
+    }
+}
+);
+
 function clickHandler() {
     var soundPath = "sounds/" + this.id +".mp3";
     var sound = new Audio(soundPath);
     sound.play();
+}
+
+function keyHandler() {
+    alert("key pressed");
+
 }
